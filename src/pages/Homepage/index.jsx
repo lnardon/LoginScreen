71153a612fb121a3148eeb5d444300a014f69b88
lnardon <lucaslnardon@gmail.com>
2021-02-11 22:14:22 -0300

    🚧 Add base form structure

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -9,7 +9,15 @@ function Homepage() {
       <div className={styles.contentContainer}>
         <div className={styles.form}>
           <h2 className={styles.formTitle}>LOGIN</h2>
-          <input type="text" />
+          <div className={styles.inputDiv}>
+            <label htmlFor="username">Username:</label>
+            <input type="text" name="username" />
+          </div>
+          <div className={styles.inputDiv}>
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" />
+          </div>
+          <button className={styles.signinBtn}>SIGN IN</button>
         </div>
       </div>
     </div>
